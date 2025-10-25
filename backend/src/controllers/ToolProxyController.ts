13a36bfec256936be7cd20f6f420a46f6a598862
Amin Vahdani <amin.vmap3240@gmail.com>
2025-10-25 11:28:31 +0330

    Delete backend directory

--- backend/src/controllers/ToolProxyController.ts
+++
@@ -1,6 +0,0 @@
-// backend/src/controllers/ToolProxyController.ts
-import { Request, Response } from 'express';
-
-export const proxyRequest = (req: Request, res: Response) => {
-  res.send('Tool proxy endpoint');
-};
